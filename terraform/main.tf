98402d61873bd29ff8314c5d0cd05771b4a2cc2d
DikshaGanchaudhuri <dganchaudhuri@gmail.com>
2026-04-10 19:48:25 +0530

    -

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,19 +50,16 @@ resource "aws_security_group" "web_sg" {
 }
 
 # EC2 Instance
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t3a.micro"
-  ...
+  ami           = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = file("${path.module}/userdata.sh")
+
+  tags = {
+    Name = "Tetris-Server"
+  }
 }
