2f9ce1ae27ac5cfcf6a2ff778bd7888dc54b0a94
DikshaGanchaudhuri <dganchaudhuri@gmail.com>
2026-04-10 19:22:29 +0530

    Fix AMI for Mumbai region (ap-south-1)

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,16 +50,19 @@ resource "aws_security_group" "web_sg" {
 }
 
 # EC2 Instance
-resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+data "aws_ami" "amazon_linux" {
+  most_recent = true
 
-  subnet_id = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  owners = ["amazon"]
 
-  user_data = file("${path.module}/userdata.sh")
-
-  tags = {
-    Name = "Tetris-Server"
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  ...
 }
