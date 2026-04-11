74773140cf446ce8de984522aec0bfbdd525926c
DikshaGanchaudhuri <dganchaudhuri@gmail.com>
2026-04-11 12:57:12 +0530

    t2.micro -> t3.micro

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,7 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0f58b397bc5c1f2e8"  
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
